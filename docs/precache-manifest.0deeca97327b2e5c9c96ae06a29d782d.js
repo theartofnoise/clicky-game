fe1933658b8ef63cb8a890d510e83419fe2cc811
Art G <theartofnoise@gmail.com>
2019-04-24 18:02:38 -0400

    build

---
+++ docs/precache-manifest.0deeca97327b2e5c9c96ae06a29d782d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5bf6180d0f05d9acabc9",
+    "url": "/static/js/main.91e623fe.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5bf6180d0f05d9acabc9",
+    "url": "/static/css/main.8acd3616.chunk.css"
+  },
+  {
+    "revision": "efa84be4bfaf74bba0ce97987af418af",
+    "url": "/index.html"
+  }
+];
